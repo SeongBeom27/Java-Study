7317bc7a4fca7379851259d5c2dd81cc47dd4f62
SeongBeom lee <52270698+SeongBeom27@users.noreply.github.com>
2022-04-06 21:04:52 +0900

    [Study] 예외 처리 학습 ( printStackTrace() )

---
+++ src/Exception/printStackTrace.java
@@ -0,0 +1,27 @@
+package Exception;
+
+class A {
+    void abc() throws NumberFormatException {
+        bcd();
+    }
+
+    void bcd() throws NumberFormatException {
+        cde();
+    }
+
+    void cde() throws NumberFormatException {
+        int num = Integer.parseInt("10A");
+    }
+}
+
+public class printStackTrace {
+    public static void main(String[] args) {
+        A a = new A();
+
+        try {
+            a.abc();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+}
