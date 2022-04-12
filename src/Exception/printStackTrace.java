e18f36bd2c4f9bdea3dfd023373703db1a0abed5
SeongBeom lee <52270698+SeongBeom27@users.noreply.github.com>
2022-04-12 20:21:30 +0900

    [Study] Generic을 사용하지 않는 이전 방식의 클래스 생성

--- src/Exception/printStackTrace.java
+++ src/Exception/printStackTrace.java
@@ -1,27 +1,27 @@
 package Exception;
 
-class A {
-    void abc() throws NumberFormatException {
-        bcd();
-    }
+// class A {
+//     void abc() throws NumberFormatException {
+//         bcd();
+//     }
 
-    void bcd() throws NumberFormatException {
-        cde();
-    }
+//     void bcd() throws NumberFormatException {
+//         cde();
+//     }
 
-    void cde() throws NumberFormatException {
-        int num = Integer.parseInt("10A");
-    }
-}
+//     void cde() throws NumberFormatException {
+//         int num = Integer.parseInt("10A");
+//     }
+// }
 
 public class printStackTrace {
     public static void main(String[] args) {
-        A a = new A();
+        // A a = new A();
 
-        try {
-            a.abc();
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
+        // try {
+        //     a.abc();
+        // } catch (NumberFormatException e) {
+        //     e.printStackTrace();
+        // }
     }
 }
